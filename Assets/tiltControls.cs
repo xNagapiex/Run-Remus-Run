cf9c8f457e862fcca0103038fa648ffd7fb1dd8e
Konttinen Taru TTK16SP <tarukonttinen@kamk.fi>
2018-01-16 10:31:28 +0200

    Gitignore fix?

---
+++ Assets/tiltControls.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
+    }
+}
