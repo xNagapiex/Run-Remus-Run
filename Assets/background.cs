ab3ef5786388bd7d5122d2604e4ccbda2cbcef16
Taru Konttinen <tarukonttinen@kamk.fi>
2018-02-13 17:26:56 +0200

    Added filler space ship, made scrolling background, confirmed accelerometer-based controls to work. Ship speed still needs to be adjusted.

---
+++ Assets/background.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour {
+
+    public float scrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.Translate(Vector3.up * scrollSpeed);
+        
+        if (transform.position.y >= 10.24f)
+        {
+            transform.position = new Vector3(transform.position.x, -10.24f, 2);
+        }
+	}
+}
